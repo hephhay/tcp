836fe249f9f67eba59782d358423016dd08fd217
hephhay <faradaydanfard@gmail.com>
2022-11-18 00:47:10 +0100

    package demonised

--- as_tcp/__main__.py
+++ as_tcp/__main__.py
@@ -1,9 +1,12 @@
 # third party libraries
 from aiorun import run
+import daemon
+
 
 # Project Modules
 from .server import serve
 from .setup import IP_ADRESS, PORT
 
 if __name__ == '__main__':
-    run(serve(IP_ADRESS, PORT))
+    with daemon.DaemonContext():
+        run(serve(IP_ADRESS, PORT))
