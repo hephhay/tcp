61679c6aed82b871fe67cc92acca3a55faa068ce
hephhay <faradaydanfard@gmail.com>
2022-11-18 01:44:07 +0100

    troubleshoot server

--- as_tcp/__main__.py
+++ as_tcp/__main__.py
@@ -8,5 +8,4 @@ from .server import serve
 from .setup import IP_ADRESS, PORT
 
 if __name__ == '__main__':
-    with daemon.DaemonContext():
-        run(serve(IP_ADRESS, PORT))
+    run(serve(IP_ADRESS, PORT))
